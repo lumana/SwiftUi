584ff7934d3921b31dd15bbecb5003dc568757d2
Luis Umaña Salas <luis.umanasalas@gmail.com>
2022-12-23 10:56:47 -0600

    Adding profile and slot machine app

---
+++ 01-ProfileApp/ProfileApp/ProfileApp/ProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppApp.swift
+//  ProfileApp
+//
+//  Created by Luis Alejandro Umana Salas on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 02-SlotMachine/Slot-Machine/Slot-Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot-Machine
+//
+//  Created by Luis Alejandro Umana Salas on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
